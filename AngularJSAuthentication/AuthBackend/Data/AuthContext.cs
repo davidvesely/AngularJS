82bcf4833be3655526bde78ea68a66e29297cdc9
davidvesely <davidvesely@gmail.com>
2015-09-16 22:34:47 +0300

    Implementing refresh tokens - in processing

--- AngularJSAuthentication/AuthBackend/Data/AuthContext.cs
+++ AngularJSAuthentication/AuthBackend/Data/AuthContext.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using AuthBackend.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -13,5 +15,8 @@ namespace AuthBackend.Data
         {
 
         }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
--- AngularJSAuthentication/AuthBackend/Data/AuthRepository.cs
+++ AngularJSAuthentication/AuthBackend/Data/AuthRepository.cs
@@ -9,18 +9,29 @@ using System.Web;
 
 namespace AuthBackend.Data
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class AuthRepository : IDisposable
     {
         private AuthContext _ctx;
 
         private UserManager<IdentityUser> _userManager;
 
+        /// <summary>
+        /// 
+        /// </summary>
         public AuthRepository()
         {
             _ctx = new AuthContext();
             _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userModel"></param>
+        /// <returns></returns>
         public async Task<IdentityResult> RegisterUser(UserModel userModel)
         {
             IdentityUser user = new IdentityUser
@@ -33,6 +44,12 @@ namespace AuthBackend.Data
             return result;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         public async Task<IdentityUser> FindUser(string userName, string password)
         {
             IdentityUser user = await _userManager.FindAsync(userName, password);
@@ -40,6 +57,91 @@ namespace AuthBackend.Data
             return user;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <returns></returns>
+        public Client FindClient(string clientId)
+        {
+            var client = _ctx.Clients.Find(clientId);
+
+            return client;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public async Task<bool> AddRefreshToken(RefreshToken token)
+        {
+
+            var existingToken = _ctx.RefreshTokens.Where(r => r.Subject == token.Subject && r.ClientId == token.ClientId).SingleOrDefault();
+
+            if (existingToken != null)
+            {
+                var result = await RemoveRefreshToken(existingToken);
+            }
+
+            _ctx.RefreshTokens.Add(token);
+
+            return await _ctx.SaveChangesAsync() > 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="refreshTokenId"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveRefreshToken(string refreshTokenId)
+        {
+            var refreshToken = await _ctx.RefreshTokens.FindAsync(refreshTokenId);
+
+            if (refreshToken != null)
+            {
+                _ctx.RefreshTokens.Remove(refreshToken);
+                return await _ctx.SaveChangesAsync() > 0;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="refreshToken"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveRefreshToken(RefreshToken refreshToken)
+        {
+            _ctx.RefreshTokens.Remove(refreshToken);
+            return await _ctx.SaveChangesAsync() > 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="refreshTokenId"></param>
+        /// <returns></returns>
+        public async Task<RefreshToken> FindRefreshToken(string refreshTokenId)
+        {
+            var refreshToken = await _ctx.RefreshTokens.FindAsync(refreshTokenId);
+
+            return refreshToken;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<RefreshToken> GetAllRefreshTokens()
+        {
+            return _ctx.RefreshTokens.ToList();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void Dispose()
         {
             _ctx.Dispose();
